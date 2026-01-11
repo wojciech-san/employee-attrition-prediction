36cc8b46098b29c1083a930fce37695aee38104e
Wojciech Czok <wojciech.czok@gmail.com>
2026-01-11 21:45:59 +0000

    added docker file

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.12-slim
+
+RUN pip install pipenv
+
+WORKDIR /app
+
+COPY ["Pipfile", "Pipfile.lock", "./"]
+RUN pipenv install --system --deploy
+
+COPY ["predict.py", "./"]
+ 
+COPY ["models/random_forest_employee_attrition_v1.bin", "./models/random_forest_employee_attrition_v1.bin"]
+
+EXPOSE 9696
+
+CMD ["uvicorn", "predict:app", "--host", "0.0.0.0", "--port", "9696"]
+
---
+++ predict-test.py
@@ -0,0 +1,22 @@
+import requests
+
+employee = {
+  "Age": 36,
+  "BusinessTravel": "Non-Travel",
+  "Department": "Research & Development",
+  "EducationField": "Medical",
+  "Gender": "Male",
+  "JobRole": "Research Scientist",
+  "MaritalStatus": "Single",
+  "TotalWorkingYears": 14.0,
+  "YearsAtCompany": 3,
+  "YearsWithCurrManager": 7,
+  "EnvironmentSatisfaction": 2.0,
+  "JobSatisfaction": 1.0,
+  "Mean_Work_Hours": 5.201124,
+  "Overtime_Days_Count": 0
+}
+
+url = 'http://localhost:9696/predict'
+response = requests.post(url, json=employee)
+print(response.json())
